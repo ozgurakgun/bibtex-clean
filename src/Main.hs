faab2335e0136ddd26b62d3a0b8009e0d0f5fbcc
Ozgur Akgun <ozgurakgun@gmail.com>
2013-12-15 23:48:32 +0000

    error if duplicate citation keys

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,11 @@ module Main where
 
 import Data.Char ( toLower )
 import Data.Ord ( comparing )
-import Data.List ( nub, sortBy )
+import Data.List ( nub, sort, sortBy, group )
+import Control.Arrow ( (&&&) )
+import Control.Monad ( unless )
+import qualified System.IO as S ( hPutStrLn, stderr )
+import qualified System.Exit as S ( exitFailure )
 
 import Text.BibTeX.Entry
 import Text.BibTeX.Parse ( file )
@@ -14,21 +18,36 @@ import Data.String.Utils ( replace )
 
 
 main :: IO ()
-main = interact $ \ stdin ->
+main = do
+    stdin <- getContents
     case parse file "<stdin>" (fixStdin stdin) of
-        Left err -> error (show err)
-        Right xs -> xs
-            |> map lowerCaseFieldNames
-            |> map lowerCaseEntryType
-            |> sortBy (comparing comp)
-            |> map entry
-            |> nub
-            |> unlines
+        Left err -> die (show err)
+        Right xs -> do
+            let
+                stdout = xs
+                    |> map lowerCaseFieldNames
+                    |> map lowerCaseEntryType
+                    |> sortBy (comparing comp)
+                    |> map entry
+                    |> nub
+                    |> unlines
+                duplicates = xs
+                    |> map identifier
+                    |> histogram
+                    |> filter (\ (_,n) -> n > 1 )
+                stderr = unlines
+                    $ "Warning: Duplicate citation keys."
+                    : map (\ (k,n) -> show n ++ "\t" ++ k ) duplicates
+
+            putStrLn stdout
+            unless (null duplicates) (die stderr)
 
 
 (|>) :: a -> (a -> b) -> b
 (|>) = flip ($)
 
+histogram :: Ord a => [a] -> [(a,Int)]
+histogram = map (head &&& length) . group . sort
 
 comp :: T -> (Maybe String, String, Maybe String, String)
 comp x = (fieldOf "year" x, entryType x, fieldOf "title" x, identifier x)
@@ -43,3 +62,7 @@ lowerCaseEntryType t = t { entryType = map toLower (entryType t) }
 fixStdin :: String -> String
 fixStdin = replace "@inproc.{" "@inproceedings{"
          . replace "@InProc.{" "@inproceedings{"
+
+die :: String -> IO ()
+die err = S.hPutStrLn S.stderr err >> S.exitFailure
+
